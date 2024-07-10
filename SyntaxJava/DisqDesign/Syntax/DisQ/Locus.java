308660938b2332fd9cda54a049456f5497b13324
saitej-avinash <saitejavinash1914@gmail.com>
2024-07-10 15:42:38 -0500

    changes

---
+++ SyntaxJava/DisqDesign/Syntax/DisQ/Locus.java
@@ -0,0 +1,13 @@
+package SyntaxJava.DisqDesign.Syntax.DisQ;
+
+// Class to define locations of qubits or groups of qubits
+class Locus {
+    private int[] indices;
+
+    public Locus(int... indices) {
+        this.indices = indices;
+    }
+
+    public int[] getIndices() { return indices; }
+}
+
---
+++ SyntaxJava/DisqDesign/Syntax/DisQ/Pair.java
@@ -0,0 +1,18 @@
+package SyntaxJava.DisqDesign.Syntax.DisQ;
+
+// A simple generic Pair class for handling tuples
+class Pair<K, V> {
+    private K key;
+    private V value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() { return key; }
+    public void setKey(K key) { this.key = key; }
+
+    public V getValue() { return value; }
+    public void setValue(V value) { this.value = value; }
+}
