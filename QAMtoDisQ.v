5257cb5dee99a53743e9133242d63eb4c60853e2
Anshu Sharma <anshu.g.sharma@gmail.com>
2024-07-21 22:34:25 -0400

    revised header for translation function

--- QAMtoDisQ.v
+++ QAMtoDisQ.v
@@ -1,10 +1,9 @@
 Require Import DisQDef.
 Require Import DisQType.
+Require Import DisQSyntax.
 Require Import QAM.
 
 (*work in progress*)
-Fixpoint QAMtoDisqTranslation (a: qam_sem) : Prop := 
-    match a with
-    | True => QAMtoDisqTranslation(~a)
-    | False => False
-    end.
+Fixpoint QAMtoDisqTranslation (a: config) : (config) * (qstate)(*input is not qam_sem*)
+  := match a with
+  | 
