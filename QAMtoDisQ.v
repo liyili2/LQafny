92f1ad9f5a35f23c7a61ae299e2a556cbe9a55d0
Anshu Sharma <anshu.g.sharma@gmail.com>
2024-08-08 09:33:18 -0400

    added left hand side of translation functions

--- QAMtoDisQ.v
+++ QAMtoDisQ.v
@@ -2,8 +2,43 @@ Require Import DisQDef.
 Require Import DisQType.
 Require Import DisQSyntax.
 Require Import QAM.
+Require Import SQIR.
 
 (*work in progress*)
-Fixpoint QAMtoDisqTranslation (a: config) : (config) * (qstate)(*input is not qam_sem*)
+(* Fixpoint QAMtoDisqTranslation (a: config) : (config) * (qstate)(*input is not qam_sem*)
   := match a with
-  | 
+  | CtxM u v => NewCMemb (QAMtoDisqTranslation u) (QAMtoDisqTranslation v)
+  end. *)
+
+Fixpoint ProcessTranslation (p: QAM.process) : (DisQSyntax.process)
+:= match p with
+| Nil => PNil
+| AR a r => AP (ActionTranslation a) (ProcessTranslation r)
+| Choice p r =>
+| Rept r =>
+end.
+
+Definition ActionTranslation (a: QAM.action) : (DisQSyntax.process)
+:= match a with
+| CreatC c => SKIP
+| QSwap c => 
+| CSend cc cm =>
+| CcRecv cc x =>
+| CqRecv qc x =>
+| LEncode q mu x =>
+| LDecode q x =>
+| GEncode c x =>
+| GDecode c x =>
+| Trans c x =>
+end.
+
+Fixpoint MembraneTranslation (m: QAM.memb) : (DisQSyntax.process)
+:= match m with 
+| CtxM r phi =>
+| ALock r t =>
+| ActM p c Q =>
+end.
+
+Fixpoint ConfigTranslation (q: config) : (DisQSyntax.process)
+| match m with 
+| a::b => 
