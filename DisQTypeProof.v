3b56bc94d0b3e7b1255430805748b58a1b3d2984
lec9243 <lec9243@gmail.com>
2024-08-12 05:33:44 -0400

    update

--- DisQTypeProof.v
+++ DisQTypeProof.v
@@ -44,26 +44,37 @@ Inductive well_dom_g : aenv -> type_map -> glocus -> Prop :=
                    @well_dom_l aenv t (map fst gl) ->
                    well_dom_g aenv t gl.                  
 
-(* redefine for type_gmap                                          
-Inductive glocus_type : type_gmap -> glocus -> se_type ->  Prop :=
-| glocus_nor : forall t l gl, In l t -> l = (gl, TNor) -> glocus_type t gl TNor
-| glocus_had : forall t l gl, In l t -> l = (gl, THad) -> glocus_type t gl THad
-| glocus_ch :  forall t l gl, In l t -> l = (gl, CH) -> glocus_type t gl CH.
+(*
+Fixpoint flatten {A : Type} (l : list (list A)) : list A :=
+  match l with
+  | [] => []
+  | x :: xs => x ++ flatten xs
+  end.
 
+Definition type_gmap2glocus (l: type_gmap):(glocus) :=
+  flatten (map (fun x => match x with (l,s,v) =>
+                                        (map (fun y => (y,v)) l) end) l).
+ *)
+
+Inductive glocus_type : type_gmap -> glocus -> se_type ->  Prop :=
+| gl_type : forall t l l' gl s v, l = map fst gl -> In l' t -> l' = (l,s,v) -> glocus_type t gl s.
 Inductive glocus_state : gqstate -> glocus -> state_elem -> Prop :=
 | gl_state : forall qs l gl s, In l qs -> l = (gl, s) -> glocus_state qs gl s. 
 
-Inductive well_form : aenv -> type_gmap -> gqstate -> Prop :=
-| well_form_nor : forall aenv t qs p r s gl, glocus_type t gl TNor -> glocus_state qs gl s -> s = (Nval p r) -> well_form aenv t qs    
-| well_form_had : forall aenv t qs b gl s, glocus_type t gl THad -> glocus_state qs gl s -> s = (Hval b) -> well_form aenv t qs
-| well_form_en : forall aenv t qs m b gl s, glocus_type t gl CH -> glocus_state qs gl s -> s = (Cval m b) -> well_form aenv t qs.
-*)
+Inductive well_formed : aenv -> type_gmap -> gqstate -> Prop :=
+| well_form_nor : forall aenv t qs p r s gl, glocus_type t gl TNor -> glocus_state qs gl s -> s = (Nval p r) -> well_formed aenv t qs    
+| well_form_had : forall aenv t qs b gl s, glocus_type t gl THad -> glocus_state qs gl s -> s = (Hval b) -> well_formed aenv t qs
+| well_form_en : forall aenv t qs m b gl s, glocus_type t gl CH -> glocus_state qs gl s -> s = (Cval m b) -> well_formed aenv t qs.
+
 
 (*Add wellformedness. well_form aenv T S is one. *)
-Theorem type_progress : forall rmax aenv T T' C S, (* well_formed aenv T S -> *)
+Theorem type_progress : forall rmax aenv T T' C S, well_formed aenv T S ->
        @c_locus_system rmax aenv T C T' -> (exists la lb S' C', @m_step rmax aenv S C la lb S' C').
 Proof.
-  intros. generalize dependent S. induction H.
+  intros. generalize dependent S. intros. induction H0.
+  destruct IHc_locus_system as [la [lb [S' [C' Hm_step]]]]. admit.
+  exists la, lb, S', C'. destruct m.
+  
 Admitted.
 
 
