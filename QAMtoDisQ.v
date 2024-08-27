3ae3bbffe998b64a819f8abe8d3ee7369f84edd6
Anshu Sharma <anshu.g.sharma@gmail.com>
2024-08-27 08:57:57 -0400

    first stab at send and receive functions

--- QAMtoDisQ.v
+++ QAMtoDisQ.v
@@ -10,23 +10,23 @@ Require Import SQIR.
   | CtxM u v => NewCMemb (QAMtoDisqTranslation u) (QAMtoDisqTranslation v)
   end. *)
 
-Fixpoint ProcessTranslation (p: QAM.process) : (DisQSyntax.process)
+(* Fixpoint ProcessTranslation (p: QAM.process) : (DisQSyntax.process)
 := match p with
 | Nil => PNil
 | AR a r => AP (ActionTranslation a) (ProcessTranslation r)
 | Choice p r =>
 | Rept r =>
-end.
+end. *)
 
 Definition ActionTranslation (a: QAM.action) : (DisQSyntax.process)
 := match a with
-| CSend cc cm =>
-| CcRecv cc x =>
-| CqRecv qc x =>
-| LEncode q mu x =>
-| LDecode q x =>
-| GEncode c x =>
-| GDecode c x =>
+| CSend cc cm => Send cc cm
+| CcRecv cc x => Recv cc x
+| CqRecv qc x => Recv cc x
+| LEncode q mu x => 
+| LDecode q x => 
+| GEncode c x => 
+| GDecode c x => 
 end.
 
 Fixpoint MembraneTranslation (m: QAM.memb) : (DisQSyntax.process)
